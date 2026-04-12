14519e448d03a4dce27e881a972039e7c6ae654e
ATHENEA20 <mermorher@gmail.com>
2026-04-11 18:54:45 -0600

    cambio1.2

---
+++ src/main/java/com/mx/ubam/controller/loteController.java
@@ -0,0 +1,51 @@
+package com.mx.ubam.controller;
+
+import com.mx.ubam.model.Lote;
+import com.mx.ubam.model.Producto;
+import com.mx.ubam.repository.LoteRepository;
+import com.mx.ubam.repository.ProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/lotes")
+@CrossOrigin(origins = "*") 
+public class loteController {
+
+    @Autowired
+    private LoteRepository loteRepo;
+
+    @Autowired
+    private ProductoRepository productoRepo;
+
+    
+    @GetMapping
+    public List<Lote> listarTodo() {
+        return loteRepo.findAll();
+    }
+    
+    @PostMapping
+    public ResponseEntity<?> guardar(@RequestBody Map<String, Object> payload) {
+        try {
+            Lote nuevoLote = new Lote();
+            nuevoLote.setCodigoLote(payload.get("codigo_lote").toString());
+            nuevoLote.setStockLote(Long.parseLong(payload.get("stock_lote").toString()));
+            nuevoLote.setFechaIngreso(java.time.LocalDate.parse(payload.get("fecha_ingreso").toString()));
+            nuevoLote.setFechaCaducidad(java.time.LocalDate.parse(payload.get("fecha_caducidad").toString()));
+
+            Integer idProd = (Integer) payload.get("id_producto");
+            Producto pro = productoRepo.findById(idProd)
+                    .orElseThrow(() -> new RuntimeException("Producto no encontrado"));
+            
+            nuevoLote.setProducto(pro);
+
+            return ResponseEntity.ok(loteRepo.save(nuevoLote));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Error: " + e.getMessage());
+        }
+    }
+}
